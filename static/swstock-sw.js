e5d69b61251ffe0dff913067cc62ff92e2f6f1b6
John Mellor <mellor@gmail.com>
2014-06-23 16:34:29 +0100

    Add Service Worker

---
+++ static/swstock-sw.js
@@ -0,0 +1,29 @@
+this.addEventListener("install", function(evt) {
+    console.log("SW oninstall");
+});
+
+this.addEventListener("activate", function(evt) {
+    console.log("SW onactivate");
+});
+
+this.addEventListener('push', function(evt) {
+    console.log("SW onpush \"" + evt.data + "\"");
+    var mayData = JSON.parse(evt.data);
+
+    var notification = new Notification("Stock price dropped", {
+        body: "FOOBAR dropped from $1030 to $" + mayData[1],
+        tag: 'stock',
+        icon: 'http://www.courtneyheard.com/wp-content/uploads/2012/10/chart-icon.png'
+    });
+    console.log(notification);
+}, false);
+
+// TODO: This might not be the right event name?
+this.addEventListener('notificationclick', function(evt) {
+    // TODO: notification.close();
+    console.log("SW notificationclick");
+    // TODO: Enumerate windows, and call window.focus(), or open a new one.
+    // TODO: drawChart(mayData) in the focused/opened tab.
+});
+
+console.log('Logged from inside SW');
