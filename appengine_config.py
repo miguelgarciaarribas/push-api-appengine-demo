14c2946f173f12100872b119bb95c6b859fb2e7a
John Mellor <johnme@chromium.org>
2015-02-12 11:43:52 +0000

    Enable appstats https://cloud.google.com/appengine/docs/python/tools/appstats

--- appengine_config.py
+++ appengine_config.py
@@ -6,3 +6,10 @@ import os.path
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 
+appstats_CALC_RPC_COSTS = True
+appstats_SHELL_OK = True
+
+def webapp_add_wsgi_middleware(app):
+  from google.appengine.ext.appstats import recording
+  app = recording.appstats_wsgi_middleware(app)
+  return app
